6cf903de6e7735ad165b23791838fdcde45bee6c
Jorge Pla <jorgepla@Jorges-MacBook-Pro.local>
2020-05-02 17:30:10 -0400

    adding buildspec file

--- lib/cdk-sqs-stack.ts
+++ lib/cdk-sqs-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from '@aws-cdk/core';
+const s3 = require('@aws-cdk/aws-s3');
 
 export class CdkSqsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const websiteBucket = new s3.Bucket(this, 'WebBucket', {
+      bucketName: 'CDKBUCKETTEST',
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true
+    });
+
   }
 }

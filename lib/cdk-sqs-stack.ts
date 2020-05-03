72fd2d040abd83fe671d708133fcfd2838fb02e7
Jorge Pla <jorgepla@Jorges-MacBook-Pro.local>
2020-05-03 12:51:19 -0400

    added lambda function

--- lib/cdk-sqs-stack.ts
+++ lib/cdk-sqs-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
 import { Queue } from '@aws-cdk/aws-sqs';
-
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class CdkSqsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -16,6 +17,18 @@ export class CdkSqsStack extends cdk.Stack {
 
     const queue = new Queue(this, 'queue', {
       queueName: 'queue'
+    });
+
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     });
 
   }
